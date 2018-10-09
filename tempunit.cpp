b777d4b481c665e5b76f277487216714cf3fbff8
Olivier FLOD Systems <olivier@flod.aero>
2018-10-09 22:03:03 +0200

    fonctions principales en place

--- tempunit.cpp
+++ tempunit.cpp
@@ -3,10 +3,12 @@
 
 TempUnit::TempUnit(){
   _currentDendriteSize = MAXSIZE;
+
   for (int i=0;i<MAXSIZE;i++){
     _dvalues[i]=0;
     _weights[i]=0;
-    _std[i]=0;
+    _std[i]=1;
+    _k[i]=0;
   }
   _nb = 0;
 }
@@ -19,23 +21,57 @@ void TempUnit::setNewTU(float *fltVector){
     _dvalues[i]= *fltVector[i];
 }
 
-float TempUnit::learnNewVector(float *fltVector){
-  float lfltScore = getScore(fltVector);
-   
-  return 0;
+int TempUnit::learnNewVector(float *fltVector, int lintReinforcement){
+  int lTmpSize = getDendriteSize();
+  float lfltTempScore, lfltTempNewK;
+  _nb++;
+
+  for (int i=0;i<lTmpSize;i++){
+    lfltTempScore = unitScore(fltVector[i],_dvalues[i],_weights[i],_std[i]);
+    lfltTempNewK = lintReinforcement * lfltTempScore/_nb;
+    _weights[i] += lfltTempNewK;
+    _dvalues[i] = (_k[i]*_dvalues[i]/(_k[i]+lfltTempNewK))+
+    (lfltTempNewK/(_k[i]+lfltTempNewK))*fltVector[i];
+    _k[i]+=lfltTempNewK;
+    _std[i]+= lintReinforcement * (0.5 - lfltTempScore)/_nb;
+  }
+  {
+    float lfltSumStd = sum(&_std);
+    float lfltSumWeight = sum(&_weights);
+    for (int i=0;i<lTmpSize;i++){
+      _std[i]/=lfltSumStd;
+      _weights/=lfltSumWeight;
+    }
+  }
+
+  return _nb;
 }
-float getScore(float *fltVector){
+
+float TempUnit::sum(float *lfltValues){
+  int lTmpSize = getDendriteSize();
+  float lftOutput=0;
+  for (int i=0;i<lTmpSize;i++)
+    lftOutput += lfltValues[i];
+  return lftOutput;
+}
+
+float TempUnit::getScore(float *fltVector){
   int lTmpSize = getDendriteSize();
   float lfltScore = 0;
-  for (int i=0;i<lTmpSize;i++){
-    lfltScore += _weights
-              *
-              (1/(_std[i]*sqrt(2*_pi)))
-              *
-              (exp(-1*(fltVector[i]-_dvalues[i])/(2*_std[i]*_std[i])));
-  }
+  for (int i=0;i<lTmpSize;i++)
+    lfltScore += unitScore(fltVector[i],_dvalues[i],_weights[i],_std[i]);
   return lfltScore;
 }
+
+float TempUnit::unitScore(float lfltInput, float lfltDendrite, float lfltWeight,
+  float lfltSigma){
+
+    return lfltWeight
+              *
+              (1/(lfltSigma*sqrt(2*_pi)))
+              *
+              (exp(-1*(lfltInput-lfltDendrite)/(2*lfltSigma]*lfltSigma)));
+  }
 
 int TempUnit::setDendriteSize(int lintSize){
   if (lintSize<MAXSIZE)
--- tempunit.h
+++ tempunit.h
@@ -17,19 +17,23 @@ class TempUnit{
   public:
     TempUnit();
     void setNewTU(float *):
-    float learnNewVector(float *);
+    int learnNewVector(float *, int lintReinforcement=1);
     int setDendriteSize(int lintSize);
     float getScore(float *);
     int getDendriteSize();
     int getMaxDSize();
 
   private:
+    float unitScore(float lfltInput, float lfltDendrite, float lfltWeight,
+      float lfltSigma);
+    float sum(float *lfltValues);
     const float _pi = 3.14159;
     float _dvalues[MAXSIZE];
     float _weights[MAXSIZE];
     float _std[MAXSIZE];
     int _nb; //nombre de fois que c'est updaté
     int _currentDendriteSize;
+    float _k[MAXSIZE]; //coefficients pour la moyenne mobile
 
 };
 
